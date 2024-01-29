60fd341101abdd1087d3c1e55739b29fe9ae2dfb
Slav <barikedoo@gmail.com>
2024-01-29 17:10:54 +0700

    upd table name

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,32 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Prisma, Task } from '@prisma/client';
+import { Prisma, task } from '@prisma/client';
 
 @Injectable()
 export class TaskService {
   constructor(private prisma: PrismaService) {}
 
-  create(data: Prisma.TaskCreateInput): Promise<Task> {
+  create(data: Prisma.taskCreateInput): Promise<task> {
     return this.prisma.task.create({ data });
   }
 
-  delete(where: Prisma.TaskWhereUniqueInput): Promise<Task> {
+  delete(where: Prisma.taskWhereUniqueInput): Promise<task> {
     return this.prisma.task.delete({ where });
   }
 
-  getById(where: Prisma.TaskWhereUniqueInput): Promise<Task | null> {
+  getById(where: Prisma.taskWhereUniqueInput): Promise<task | null> {
     return this.prisma.task.findUnique({ where });
   }
 
   update(params: {
-    where: Prisma.TaskWhereUniqueInput;
-    data: Prisma.TaskUpdateInput;
-  }): Promise<Task> {
+    where: Prisma.taskWhereUniqueInput;
+    data: Prisma.taskUpdateInput;
+  }): Promise<task> {
     const { where, data } = params;
     return this.prisma.task.update({ data, where });
   }
 
-  getAll(): Promise<Task[]> {
+  getAll(): Promise<task[]> {
     return this.prisma.task.findMany();
   }
 }
